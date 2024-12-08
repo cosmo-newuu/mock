db7fab75b88b094d6dfae847067d09e94cf950c7
Javoxir Ismoilov <j.ismoilov@newuu.uz>
2024-12-08 11:30:30 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 
-int findMax(const vector<int>& arr) {
+int findMax(int* arr, int size) {
     int max = arr[0];
-    for (size_t i = 1; i < arr.size(); i++) {
+    for (int i = 1; i < size; i++) {
         if (arr[i] > max) {
             max = arr[i];
         }
@@ -13,69 +12,111 @@ int findMax(const vector<int>& arr) {
     return max;
 }
 
-void reverseArray(vector<int>& arr) {
-    for (size_t i = 0; i < arr.size() / 2; i++) {
-        swap(arr[i], arr[arr.size() - i - 1]);
+
+void reverseArray(int* arr, int size) {
+    for (int i = 0; i < size / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[size - i - 1];
+        arr[size - i - 1] = temp;
     }
 }
 
 
-double calculateAverage(const vector<int>& arr) {
+double calculateAverage(int* arr, int size) {
     int sum = 0;
-    for (int num : arr) {
-        sum += num;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
     }
-    return sum / (double)arr.size();
+    return sum / (double)size; 
 }
 
 int main() {
-    vector<int> arr;
-    int num;
+    int* arr = nullptr;
+    int num, size = 0, capacity = 10;
 
-    
+    arr = new int[capacity];
+
+
     while (cin >> num && num != -1) {
-        arr.push_back(num);
+        if (size == capacity) {
+          
+            capacity *= 2;
+            int* newArr = new int[capacity];
+            for (int i = 0; i < size; i++) {
+                newArr[i] = arr[i];
+            }
+            delete[] arr;
+            arr = newArr;
+        }
+        arr[size++] = num;
     }
 
-
-    if (!arr.empty()) {
-        cout << "Maximum value: " << findMax(arr) << endl;
+   
+    if (size > 0) {
+        cout << "Maximum value: " << findMax(arr, size) << endl;
     } else {
         cout << "No elements entered." << endl;
     }
 
+    delete[] arr;
+    arr = new int[capacity];
+    size = 0;
 
-    arr.clear();
-
+  
     while (cin >> num && num != -1) {
-        arr.push_back(num);
+        if (size == capacity) {
+            capacity *= 2;
+            int* newArr = new int[capacity];
+            for (int i = 0; i < size; i++) {
+                newArr[i] = arr[i];
+            }
+            delete[] arr;
+            arr = newArr;
+        }
+        arr[size++] = num;
     }
 
 
-    if (!arr.empty()) {
-        reverseArray(arr);
+    if (size > 0) {
+        reverseArray(arr, size);
         cout << "Reversed array: ";
-        for (int val : arr) {
-            cout << val << " ";
+        for (int i = 0; i < size; i++) {
+            cout << arr[i] << " ";
         }
         cout << endl;
     } else {
         cout << "No elements entered." << endl;
     }
 
+    
+    delete[] arr;
+    arr = new int[capacity];
+    size = 0;
 
-    arr.clear();
-
+   
+    
     while (cin >> num && num != -1) {
-        arr.push_back(num);
+        if (size == capacity) {
+            capacity *= 2;
+            int* newArr = new int[capacity];
+            for (int i = 0; i < size; i++) {
+                newArr[i] = arr[i];
+            }
+            delete[] arr;
+            arr = newArr;
+        }
+        arr[size++] = num;
     }
 
-
-    if (!arr.empty()) {
-        cout << "Average: " << calculateAverage(arr) << endl;
+   
+    if (size > 0) {
+        cout << "Average: " << calculateAverage(arr, size) << endl;
     } else {
         cout << "No elements entered." << endl;
     }
+
+    
+    delete[] arr;
 
     return 0;
 }
