9c91051c70b36d412327df9ce89a3251ce97bfe6
Javoxir Ismoilov <j.ismoilov@newuu.uz>
2024-12-08 11:13:44 +0500

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int sumEvenNumbers() {
     int sum = 0, num;
+    cout << "Enter numbers (-1 to end): ";
     while (cin >> num && num != -1) {
         if (num % 2 == 0) sum += num;
     }
     return sum;
 }
 
+
 int longestConsecutiveSubsequence() {
-    int num, prevnum, currentLength = 1, maxLength = 0;
-    cin >> prevnum;
-    if (prevnum == -1) return 0;
+    int num, prevNum, currentLength = 1, maxLength = 1;
+    cout << "Enter numbers (-1 to end): ";
+    
+    cin >> prevNum;
+    if (prevNum == -1) return 0;
+
     while (cin >> num && num != -1) {
-        if (num == prevnum) {
+        if (num == prevNum) {
             currentLength++;
         } else {
             maxLength = max(maxLength, currentLength);
             currentLength = 1;
         }
-        prevnum = num;
+        prevNum = num;
     }
+
     maxLength = max(maxLength, currentLength);
     return maxLength;
 }
 
 int main() {
     cout << "Sum of even numbers: " << sumEvenNumbers() << endl;
-    cout << "Length of longest consecutive subsequence: " << longestConsecutiveSubsequence() << endl;
+    cout << "Length of longest consecutive subsequence of the same number: " << longestConsecutiveSubsequence() << endl;
     return 0;
 }
